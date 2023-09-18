627b6b80bd487c254df66d691a4e64bbac87df3c
Gustavo André Richter <gustavoarichter@gmail.com>
2023-09-17 22:28:01 -0300

    wip: Componente barra lateral.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Alura-Tracker
  Projeto do programa de aceleração Dev do Bling
+
+ Para rodar o projeto: npm rum serve
--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -1,10 +1,22 @@
-<template></template>
+<template>
+	<main class="columns is-gapless is-multiline">
+		<div class="column is-one-quarter">
+			<BarraLateral/>
+		</div>
+
+		<div class="column is-three-quarters">
+
+		</div>
+	</main>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
 
 export default defineComponent({
-	name: 'App'
+    name: 'App',
+    components: { BarraLateral }
 });
 </script>
 
