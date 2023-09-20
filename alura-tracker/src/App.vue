7115f2347eecd95b0ec7993b712fa0ec9ceb51bb
Gustavo André Richter <gustavoarichter@gmail.com>
2023-09-20 13:21:49 -0300

    wip: add formulario na tela.

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -5,7 +5,7 @@
 		</div>
 
 		<div class="column is-three-quarters">
-
+			<Formulario/>
 		</div>
 	</main>
 </template>
@@ -13,10 +13,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue';
 
 export default defineComponent({
-    name: 'App',
-    components: { BarraLateral }
+	name: 'App',
+	components: { 
+		BarraLateral, 
+		Formulario 
+	}
 });
 </script>
 
