9f73f207354da6e3124fff6a91284d285da11f13
Gustavo André Richter <gustavoarichter@gmail.com>
2023-09-21 20:36:25 -0300

    wip: add vue-route

--- alura-tracker/src/App.vue
+++ alura-tracker/src/App.vue
@@ -5,13 +5,7 @@
 		</div>
 
 		<div class="column is-three-quarters conteudo">
-			<Formulario @aoSalvarTarefa="salvarTarefa"/>
-			<div class="lista">
-				<tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-				<Box v-if="listaVazia">
-					Você não possui tarefas!
-				</Box>
-			</div>
+			<router-view></router-view>
 		</div>
 	</main>
 </template>
@@ -19,34 +13,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import Formulario from './components/Formulario.vue';
-import Tarefa from './components/Tarefa.vue';
-import ITarefa from './interfaces/ITarefa'
-import Box from './components/Box.vue';
 
 export default defineComponent({
 	name: 'App',
 	components: {
 	BarraLateral,
-	Formulario,
-	Tarefa,
-	Box
 },
 	data() {
 		return {
-			tarefas: [] as ITarefa[],
 			modoEscuroAtivo: false
 		}
 	},
-	computed: {
-		listaVazia(): boolean {
-			return this.tarefas.length === 0
-		}
-	},
 	methods: {
-		salvarTarefa(tarefa: ITarefa) {
-			this.tarefas.push(tarefa);
-		},
 		trocarTema(modoEscuroAtivo: boolean) {
 			this.modoEscuroAtivo = modoEscuroAtivo
 		}
