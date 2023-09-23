7de4ea62b23cd7c27f9518d240a00b1d275ea54f
Gustavo André Richter <gustavoarichter@gmail.com>
2023-09-23 19:59:23 -0300

    wip: add projetos manualmente.

--- alura-tracker/src/store/index.ts
+++ alura-tracker/src/store/index.ts
@@ -1,7 +1,6 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
-import { createStore } from "vuex";
-import { Store } from "vuex";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
 
 interface Estado {
 	projetos: IProjeto[]
@@ -12,18 +11,20 @@ export const key: InjectionKey<Store<Estado>> = Symbol();
 export const store = createStore<Estado>({
 	state: {
 		projetos: [
-			{
-				id: new Date().toISOString(),
-				nome: 'TypeScript'
-			},
-			{
-				id: new Date().toISOString(),
-				nome: 'Vue'
-			},
-			{
-				id: new Date().toISOString(),
-				nome: 'Vuex'
-			}
+			
 		]
+	},
+	mutations: {
+		'ADICIONA_PROJETOS'(state, nomeDoProjeto: string) {
+			const projeto = {
+				id: new Date().toISOString(),
+				nome: nomeDoProjeto
+			} as IProjeto
+			state.projetos.push(projeto)
+		}
 	}
 })
+
+export function useStore(): Store<Estado> {
+	return vuexUseStore(key);
+}
